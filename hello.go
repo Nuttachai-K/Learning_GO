ffc040191f0ec9f5332e501f8f945c4d599dc527
Nuttachai-K <korn.nuttachai.jp@gmail.com>
2022-09-13 20:59:13 +0700

    go test

--- hello.go
+++ hello.go
@@ -15,14 +15,20 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Gladys")
+	//A slice of name
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	//Request greeting message for each name.
+	messages, err := greetings.Hellos(names)
+
+	//message, err := greetings.Hello("Gladys")
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message
+	// If no error was returned, print the returned map of message
 	// to the console.
-	fmt.Println(message)
+	fmt.Println(messages)
+
 }
